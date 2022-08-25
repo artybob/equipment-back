2d87df6654f6b6466af40a34528c7aa7ace3066e
ArtyBob <nova1hazard@gmail.com>
2022-08-26 01:44:39 +0700

    fixes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,13 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-         \App\Models\User::factory(2)->create();
+        \App\Models\User::factory(2)->create();
         \App\Models\Equipment::factory(10)->create();
         \App\Models\EquipmentType::factory(5)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
